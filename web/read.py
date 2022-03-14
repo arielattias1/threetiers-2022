7bc5b2fdff0321828dbc0c145d7a54e7c9d2aaf8
arielattias1 <74975408+arielattias1@users.noreply.github.com>
2022-03-14 12:04:02 -0400

    Add files via upload

--- web/read.py
+++ web/read.py
@@ -3,7 +3,7 @@
 # pip install flask-mysql
 
 # imports
-from flask import Flask
+from flask import Flask,render_template
 from flaskext.mysql import MySQL
 
 # web application
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # connect to db
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER']     = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'MyNewPass'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Rh9thm&P0etr9'
 app.config['MYSQL_DATABASE_DB']       = 'education'
 app.config['MYSQL_DATABASE_HOST']     = 'localhost'
 mysql.init_app(app)
@@ -21,3 +21,19 @@ mysql.init_app(app)
 #           YOUR CODE
 # ----------------------------------- 
 
+#create route 
+
+@app.route('/colleges')
+def colleges():
+    cursor = mysql.get_db().cursor()
+    response = cursor.execute('SELECT * FROM Colleges')
+    html = ''
+    if response> 0: 
+        return render_template('colleges.html',list = colleges)
+
+
+
+#start server 
+
+if __name__ == '__main__':
+    app.run(debug =True , port=3000)
